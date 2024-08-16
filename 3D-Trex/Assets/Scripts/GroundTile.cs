99d38ec0dbde8f8f3222e703a854dc2cb18ff06d
zyon213 <yg.mms2@gmail.com>
2024-08-16 20:09:42 +0400

    Trex game added

---
+++ 3D-Trex/Assets/Scripts/GroundTile.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+    [SerializeField]  GameObject obstaclePrefab;
+    [SerializeField] GameObject tallObstaclePrefab;
+    [SerializeField] float tallObstacleChance = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnObstacle();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        GameObject obstacleToSpawn = obstaclePrefab;
+        float random = Random.Range(0f, 1f);
+
+        if (random < tallObstacleChance)
+            obstacleToSpawn = tallObstaclePrefab;
+
+        Transform spawnPoint = transform.GetChild(2).transform;
+
+        Instantiate(obstacleToSpawn, spawnPoint.position, Quaternion.identity, transform);
+    }
+}
---
+++ 3D-Trex/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    PlayerMovement playerMovement;
+    void Start()
+    {
+        playerMovement = GameObject.FindObjectOfType<PlayerMovement>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerMovement.Die();
+        }
+    }
+    // Update is called once per frame
+}
